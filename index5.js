26c965f8c0077c0d97f003c0a3c0eca88473995b
alvinashia <30523alvina@gmail.com>
2022-04-11 23:28:02 +0800

    add function test (return)

--- index5.js
+++ index5.js
@@ -1,9 +1,31 @@
 
-function hello(){
-    console.log('你好')}
+// //宣告
+// function hello(){
+//     console.log('你好')}
     
-function hello(){
-    console.log('你好')}
+// function hello(){
+//     console.log('你好2')}
     
-function hello(){
-    console.log('你好')}
+// function hello(){
+//     console.log('你好3')}
+
+
+// //執行
+// hello()
+// hello2()
+// hello3()
+
+
+function addMoney(price1,price2,discount){
+    let result = price1+price2-discount
+  return result
+}
+
+let total = addMoney(3000,200,1000)
+console.log('total',total);
+
+// //結賬頁面
+// addMoney(2000,100)
+
+// //購物車頁面
+// addMoney(3000,200)
